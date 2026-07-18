459ac8ced1f493926e735b316cfd435f858a3fde
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-18 15:12:51 +0100

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,21 @@
+function calculatorReply(msg){
 
+    if(!/^[0-9+\-*/(). x]+$/i.test(msg)){
+        return null;
+    }
+
+    try{
+
+        let expression = msg.replace(/x/gi,"*");
+
+        let result = eval(expression);
+
+        return "🧮 Answer: <strong>" + result + "</strong>";
+
+    }catch{
+
+        return "❌ Sorry, I couldn't calculate that.";
+
+    }
+
+}
