514b35d65ba8f74025b879425207be426c22393d
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-19 16:47:03 +0100

    Update notes.js

--- js/notes.js
+++ js/notes.js
@@ -1,7 +1,13 @@
 alert("NOTES FILE LOADED");
+
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
 
 function noteReply(msg, text){
+
+    alert("Inside noteReply");
+
+    return "📝 noteReply works!";
+}
 
     // Create Note
     if(
