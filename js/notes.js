9a4bf9f2d4b32c167e0ead6b549b569338ebd113
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-19 18:44:47 +0100

    Add alert for received message in noteReply function

--- js/notes.js
+++ js/notes.js
@@ -3,7 +3,7 @@ alert("NOTES FILE LOADED");
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
 
 function noteReply(msg, text){
-
+alert("Message received: " + msg);
     // Save Note
     if(
         msg.startsWith("create a note ") ||
