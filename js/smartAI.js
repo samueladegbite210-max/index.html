26e54dc7d6ca2bbdbd9821f2fdb761f59329e7c0
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-23 21:24:14 +0100

    Update smartAI.js

--- js/smartAI.js
+++ js/smartAI.js
@@ -10,67 +10,67 @@ async function smartAIReply(msg){
         return null;
     }
 
-    // ==========================
-    // LOCAL MODULES
-    // ==========================
-
     let answer = null;
 
+    // Conversation
     answer = conversationReply(msg);
     if(answer) return answer;
 
-    answer = knowledgeReply(msg);
-    if(answer) return answer;
-
-    answer = calculatorReply(msg);
-    if(answer) return answer;
-
-    answer = dateTimeReply(msg);
-    if(answer) return answer;
-
-    answer = taskReply(msg, msg);
-    if(answer) return answer;
-
-    answer = goalReply(msg, msg);
-    if(answer) return answer;
-
-    answer = noteReply(msg, msg);
-    if(answer) return answer;
-
-    answer = eventReply(msg, msg);
-    if(answer) return answer;
-
+    // Memory (move this near the top)
     answer = memoryReply(msg, msg);
     if(answer) return answer;
 
+    // Knowledge
+    answer = knowledgeReply(msg);
+    if(answer) return answer;
+
+    // Calculator
+    answer = calculatorReply(msg);
+    if(answer) return answer;
+
+    // Date & Time
+    answer = dateTimeReply(msg);
+    if(answer) return answer;
+
+    // Tasks
+    answer = taskReply(msg, msg);
+    if(answer) return answer;
+
+    // Goals
+    answer = goalReply(msg, msg);
+    if(answer) return answer;
+
+    // Notes
+    answer = noteReply(msg, msg);
+    if(answer) return answer;
+
+    // Events
+    answer = eventReply(msg, msg);
+    if(answer) return answer;
+
+    // Natural Conversation
     answer = naturalReply(msg);
     if(answer) return answer;
 
+    // Food
     answer = foodReply(msg);
     if(answer) return answer;
 
+    // Weather
     answer = weatherReply(msg);
     if(answer) return answer;
 
+    // Brain
     answer = aiBrainReply(msg);
     if(answer) return answer;
 
+    // Advice
     answer = adviceReply(msg);
     if(answer) return answer;
 
-    // ==========================
-    // INTERNET SEARCH
-    // ==========================
-
+    // Internet
     answer = await internetReply(msg);
-
-    if(answer){
-        return answer;
-    }
-
-    // ==========================
-    // DEFAULT
-    // ==========================
+    if(answer) return answer;
 
     return "🤖 I couldn't find an answer yet. Try asking another question.";
 
